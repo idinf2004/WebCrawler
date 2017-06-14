0b0f4c38f3d00413a71a82a2fc205f55285be4d5
idinf2004 <idinf2004@gmail.com>
2017-06-14 11:36:23 +0100

    add fileHandler posirive Test

---
+++ WebCrawlerTests/FileHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebCrawler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Tests
+{
+    [TestClass()]
+    public class FileHandlerTests
+    {
+        private FileHandler _fileHandler;
+        public FileHandlerTests()
+        {
+            _fileHandler = new FileHandler();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\Resources\";
+            _fileHandler.Save("txt","test", path);
+            bool expected = File.Exists(_fileHandler.GetFilePath);
+            Assert.IsTrue(expected);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            if (File.Exists(_fileHandler.GetFilePath))
+            {
+                File.Delete(_fileHandler.GetFilePath);
+            }
+        }
+
+    }
+}
